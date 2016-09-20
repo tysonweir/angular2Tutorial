5cc9376ac044738307b1861e6ab2a2e6bcf289ea
Tyson Hughes <tdhughes@xactware.com>
2016-09-20 14:24:44 -0600

    Changes to the webpack configs

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -28,12 +28,12 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                exclude: helpers.root('src', 'app'),
+                exclude: helpers.root('app'),
                 loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
             },
             {
                 test: /\.css$/,
-                include: helpers.root('src', 'app'),
+                include: helpers.root('app'),
                 loader: 'raw'
             }
         ]
